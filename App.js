708ee79c971dfbcbfe1925f7e3b2ed6d3065ed54
DougTheBoy <101404920+DougTheBoy@users.noreply.github.com>
2023-04-07 00:37:01 -0300

    Projeto React

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MeuComponente from './MeuComponente';
+
+function App() {
+  return (
+    <div>
+      <MeuComponente />
+    </div>
+  );
+}
+
+export default App;
+
